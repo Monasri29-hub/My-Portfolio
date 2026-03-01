eee59aaded4e79b2265b969e2dd702eeaba4826a
Monasri29-hub <monasri9c.vhs@gmail.com>
2026-03-01 11:59:05 +0530

    Configure building and routing for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -43,6 +43,10 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+export default app;
